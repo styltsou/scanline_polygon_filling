5b731b26d4e715e9577dfabee2b2d85d529de798
styltsou <tsoumanis.stylianos@gmail.com>
2022-04-09 05:17:27 +0300

    Fixed some bugs. The aglorithm doesn't render the object properly

--- demo_flat.py
+++ demo_flat.py
@@ -16,11 +16,13 @@ if __name__ == "__main__":
 
     img = render(verts2d, faces, vcolors, depth, 'flat')
     
+    print(img.shape)
+    
     # convert image from BGR to RGB format
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    # img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     
     # show image using pyplot
-    plt.imshow(img)
+    # plt.imshow(img)
     
     # convert img colors from range [0,1] to range [0,255] before saving
-    # cv2.imwrite('flat_shading.png', img * 255)
+    cv2.imwrite('flat_shading.png', img * 255)
--- shade_triangle.py
+++ shade_triangle.py
@@ -4,6 +4,8 @@ from Edge import Edge
 from interpolate_color import interpolate_color
 
 def shade_triangle(img, verts2d, vcolors, shade_t):
+  Y = img
+  
   ymin = np.min(verts2d[:, 1])
   ymax = np.max(verts2d[:, 1])
   
@@ -13,8 +15,8 @@ def shade_triangle(img, verts2d, vcolors, shade_t):
   # Edge bucket is a structure that contains information about an edge
   # define edge bucket data type
   edge_bucket_dtype = np.dtype({
-    'names': ('ymin, ymax', 'slope', 'intersection'),
-    'formats': (int, int, np.float, int)
+    'names': ('ymin', 'ymax', 'slope', 'intersection'),
+    'formats': (int, int, np.float32, np.float32)
   })
   
   edge_buckets = np.array(
@@ -26,36 +28,40 @@ def shade_triangle(img, verts2d, vcolors, shade_t):
   #   [(edge.ymin, edge.ymax, edge.slope(), edge.x_of_ymin) for edge in edges if not edge.is_horizontal() if edge.ymin == ymin],
   #   dtype=edge_bucket_dtype)
   
-  ActiveEdgesList = edge_buckets[(edge_buckets['slope'] != 0) and (edge_buckets['ymin'] == ymin)]
+  ActiveEdgesList = edge_buckets[np.logical_and(edge_buckets['slope'] != 0, edge_buckets['ymin'] == ymin)]
+  # ActiveEdgesList = edge_buckets[edge_buckets['ymin'] == ymin]
+  # ActiveEdgesList = ActiveEdgesList[ActiveEdgesList['slope'] != 0]
   
   for y in range(ymin, ymax):
     # sort Active Edges in respect to intersection
     ActiveEdgesList = ActiveEdgesList[np.argsort(ActiveEdgesList['intersection'])]
     
-    for i in range(ActiveEdgesList.shape[0]):
+    for i in range(ActiveEdgesList.shape[0] - 1):
       # do a for loop here
       if shade_t == 'flat':
-        for x in range(ActiveEdgesList[i]['intersection'], ActiveEdgesList[i + 1]['intersection']):
+        for x in range(int(ActiveEdgesList[i]['intersection']), int(ActiveEdgesList[i + 1]['intersection'])):
           # paint pixel using the center mass of vetrices colors
-          img[y, x] = np.sum(vcolors, axis=1) / 3 
+          Y[y, x] = np.sum(vcolors, axis=1) / 3 
       elif shade_t == 'gouraud':
         # Add a comment here
         color_left = interpolate_color()
         color_right = interpolate_color()
         
-        for x in range(ActiveEdgesList[i]['intersection'], ActiveEdgesList[i + 1]['intersection']):
-          img[x, y] = interpolate_color(0, 0, x, color_left, color_right)
+        for x in range(int(ActiveEdgesList[i]['intersection']), int(ActiveEdgesList[i + 1]['intersection'])):
+          Y[x, y] = interpolate_color(0, 0, x, color_left, color_right)
     
     # Update ActiveEdgesList (AEL) recursively
     # Exclude edges where ymax == y
     ActiveEdgesList = ActiveEdgesList[ActiveEdgesList['ymax'] != y]
     
     # update intersection for pre-existing edges
-    ActiveEdgesList['intersection'] = ActiveEdgesList['intersection'] + 1 / ActiveEdgesList['slope']
+    ActiveEdgesList['intersection'] = ActiveEdgesList['intersection'] + (1 / ActiveEdgesList['slope'])
     
     # Add edges where ymin == y + 1 in AEL
     newEdges = edge_buckets[edge_buckets['ymin'] == y + 1]
     ActiveEdgesList = np.append(ActiveEdgesList, newEdges)
     
-    # return img 
-    return img
+  # Do I need extra code to handle horizontal edges??
+  
+  # return img 
+  return Y
