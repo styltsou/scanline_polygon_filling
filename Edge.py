c8eb3a246e4fe417e4c05d65cdd8bab975a0c128
styltsou <tsoumanis.stylianos@gmail.com>
2022-04-10 18:26:23 +0300

    Added some comments

--- Edge.py
+++ Edge.py
@@ -7,9 +7,12 @@ class Edge:
     self.ymin = min(verts2d[:, 1])
     self.ymax = max(verts2d[:, 1])
     self.x_of_ymin = verts2d[np.argmin(verts2d[:, 1]), 0]
+    # Color of edge's lower vertex
     self.low_vert_color = vcolors[np.argmin(verts2d[:, 1])]
+    # Color of edge's higher vertex
     self.high_vert_color = vcolors[np.argmax(verts2d[:, 1])]
     
+  # returns True if the edge is vertical
   def is_vertical(self):
     return self.point_a[0] == self.point_b[0]
   
